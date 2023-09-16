4d0e0db9b22da4b5660ce000b690919a2f4b7c92
Paz Valenzuela <pazvalenzu@gmail.com>
2023-09-16 03:40:17 +0000

    commitreal

--- src/models.py
+++ src/models.py
@@ -1,43 +1,90 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine, DateTime, Boolean
+from sqlalchemy.orm import relationship, declarative_base, backref
+from eralchemy2 import render_er
 from datetime import datetime
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(100), nullable=False)  # La contraseña debería estar cifrada
-    name = Column(String(100))
-    registration_date = Column(DateTime, default=datetime.utcnow)
-    profile_picture = Column(String(255))
-    followers_count = Column(Integer, default=0)
-    following_count = Column(Integer, default=0)
+    ID = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Friend(Base):
+    __tablename__ = 'friend'
+    user_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
+    friend_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
+
+class PrivateMessage(Base):
+    __tablename__ = 'private_message'
+    message_id = Column(Integer, primary_key=True)
+    sender_id = Column(Integer, ForeignKey('user.ID'))
+    receiver_id = Column(Integer, ForeignKey('user.ID'))
+    message_text = Column(String(250), nullable=False)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+class Interaction(Base):
+    __tablename__ = 'interaction'
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.ID'))
+    post_id = Column(Integer, ForeignKey('post.ID'))
+    interaction_type = Column(String(50))  # Puede ser 'like', 'share', etc.
+
+class Hashtag(Base):
+    __tablename__ = 'hashtag'
+    ID = Column(Integer, primary_key=True)
+    tag_name = Column(String(100), unique=True)
+
+class PostHashtag(Base):
+    __tablename__ = 'post_hashtag'
+    post_id = Column(Integer, ForeignKey('post.ID'), primary_key=True)
+    hashtag_id = Column(Integer, ForeignKey('hashtag.ID'), primary_key=True)
+
+class Notification(Base):
+    __tablename__ = 'notification'
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.ID'))
+    notification_text = Column(String(250), nullable=False)
+    is_read = Column(Boolean, default=False)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.ID'), primary_key=True)
+    user_from = relationship("User", foreign_keys=[user_from_id], backref=backref("following", uselist=True))
+    user_to = relationship("User", foreign_keys=[user_to_id], backref=backref("followed_by", uselist=True))
+
+class Media(Base):
+    __tablename__ = 'media'
+    ID = Column(Integer, primary_key=True)
+    type = Column(Integer)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.ID'))
+    post = relationship("Post", backref=backref("media", uselist=True))
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    caption = Column(String(280))
-    image_url = Column(String(255))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.ID'))
+    user = relationship("User", backref="posts")  # Relación con User
 
 class Comment(Base):
     __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True)
-    text = Column(String(280))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-    user = relationship(User)
-    post = relationship(Post)
+    ID = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.ID'))
+    post_id = Column(Integer, ForeignKey('post.ID'))
+    post = relationship("Post", backref="comments")  # Relación con Post
+    author = relationship("User", backref="comments")  # Relación con User
 
-class Like(Base):
-    __tablename__ = 'like'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-    user = relationship(User)
-    post = relationship(Post)
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem generating the diagram")
+    raise e
